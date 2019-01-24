937fb1e8587072f5def0727d6b868a354d078512
Agustin Allera <agustin.allera@gmail.com>
2019-01-24 11:05:43 -0300

    mod5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
