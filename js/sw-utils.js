3e514a83595850bafec1bfdd2651d2f29c68490d
Agustin Allera <agustin.allera@gmail.com>
2019-01-24 10:17:48 -0300

    pr

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+function actualizarCacheDinamico(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+};
